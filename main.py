7553d46ff1b640e8478d5ec6f8130bd1267837ff
yusuf <af003yusuf@gmail.com>
2022-09-25 19:26:32 +0700

    .

--- main.py
+++ main.py
@@ -6,21 +6,5 @@ from pydantic import BaseModel
 #serialisasi : proses convert format data ke bentuk yang bisa dikirim lewat API (kebanyakan JSON)
 app = FastAPI()
 
-dataBuku = '''{'id':'1','author':'kucing','title':'ini buku','desc':'ini deskripsi','cover_img':'linknya'}'''
-#class harus huruf besar awalnya
 class Buku(BaseModel):
-    id:int
-    author:str
-    title:str
-    desc:str
-    cover:str
-
-@app.get("/")
-async def getBuku(id,author,title,desc,cover):
-    results = {'id':id,'author':author,'title':title,'desc':desc,'cover':cover}
-    return results
-
-@app.post("/")
-async def postBuku(id,author,title,desc,cover):
-    results = {'id':id,'author':author,'title':title,'desc':desc,'cover':cover}
-    return results
+    
